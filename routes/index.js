b3cd138479e052c8ac5cdd3308488b2cf80bb8f9
davidigandan <69117955+davidigandan@users.noreply.github.com>
2023-11-03 13:48:50 +0000

    Basic Programmatic Implementation of API

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router(); 
 const youtubedl = require('youtube-dl-exec')
+const request = require('request');
+const app = express()
+const path = require('path');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 
 
@@ -9,6 +16,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Convert vido url to mp3
 router.post("/convert-mp3", async (req, res) => {
 
   const videoUrl = req.body.url;
@@ -26,4 +34,36 @@ router.post("/convert-mp3", async (req, res) => {
     console.error('Error downloading video:', error);
   });
 })
+
+// Transcribe the video
+const audioFileName = req.query.audioFileName;
+
+app.post('/transcribe', (req, res) => {
+  const audioFilePath = path.join(__dirname, 'downloads', audioFileName );
+
+  const audioFile = fs.createReadStream(audioFilePath);
+
+  const endpoint = process.env.AZURE_SPEECH_ENDPOINT;
+  const apiKey = process.env.AZURE_SPEECH_KEY;
+
+  const requestOptions = {
+    url: `${endpoint}/speech/recognition/v3.0/transcriptions?language=en-US&profanity=Raw`,
+    method: 'POST',
+    headers: {
+      'Ocp-Apim-Subscription-Key': apiKey,
+      'Content-Type': 'audio/mpeg',
+    },
+    body: audioFile,
+  };
+
+  request(requestOptions, (error, response, body) => {
+    if (error) {
+      res.status(500).send('Error trnascribing audio.');
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
+});
+
+
 module.exports = router;
